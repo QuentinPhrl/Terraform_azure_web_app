fd5791983b8fb1d309b2c7a1e0d534c37e9e4f2f
QuentinP <puechguiral81@hotmail.fr>
2023-03-11 16:58:37 +0100

    init

---
+++ main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rpz_rg" {
+  name     = "rpz-resources"
+  location = "West Europe"
+  tags     = {
+    "env" = "dev"
+  }
+}
+
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "webapp-asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rpz_rg.location
+  resource_group_name = azurerm_resource_group.rpz_rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "webapp-${random_integer.ri.result}"
+  location              = azurerm_resource_group.rpz_rg.location
+  resource_group_name   = azurerm_resource_group.rpz_rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
+    application_stack {
+        node_version = "16-lts"
+    }
+  }
+}
